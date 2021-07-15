27cbde7dd06a51c18587b1fce30a9f323c643455
naming123 <79265793+naming123@users.noreply.github.com>
2021-07-16 04:18:00 +0900

    mobile버젼만들고있는중

---
+++ start.js
@@ -0,0 +1,9 @@
+const simli1 =document.querySelector("simli1.html")
+const simli2 =document.querySelector("simli2.html")
+
+function begin(){
+    
+    simli1.style.display="none";
+    simli2.style.display="block";
+
+}
